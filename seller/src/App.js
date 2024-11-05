150da03c3ced17f83975bcbc26af1cad9e18c736
pri2003yam <132195934+pri2003yam@users.noreply.github.com>
2024-11-05 11:31:39 +0530

    home

--- seller/src/App.js
+++ seller/src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 import LoginPage from './pages/loginpage';
 import Registerpage from './pages/registerpage';
 import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Navbar from './componenets/Navbar';
+import Carousal from './componenets/carousal';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
        <BrowserRouter>
        <Routes>
        <Route path = "/login" element = {<LoginPage/>}/>
